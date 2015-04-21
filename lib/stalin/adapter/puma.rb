1be44076254b8b6799200e2a3280eab186b12896
Tony Spataro <tony@rightscale.com>
2015-04-21 16:51:42 -0700

    Make ourselves compatible with puma-via-rackup.

--- lib/stalin/adapter/puma.rb
+++ lib/stalin/adapter/puma.rb
@@ -1,6 +1,8 @@
 module Stalin::Adapter
-  # A Stalin adapter that is suitable for use in Puma worker processes. Puma uses SIGTERM
-  # to initiate orderly shutdown and seems to respond to SIGQUIT with an abrupt shutdown.
+  # A Stalin adapter that is suitable for use in Puma worker processes. Puma has two
+  # execution models.
+  # - "Cluster" spawns N worker processes; each responds to SIGTERM with graceful shutdown and SIGQUIT with abrupt shutdown
+  # - "Single" runs a server in the one-and-only master process; it responds to SIGUSR2 with graceflu restart
   class Puma < Rack
     # Create a middleware instance.
     #
@@ -10,17 +12,19 @@ module Stalin::Adapter
     # @param [Integer] cycle how frequently to check memory consumption (# requests)
     # @param [Boolean] verbose log extra information
     def initialize(app, min=1024**3, max=2*1024**3, cycle=16, verbose=false)
+      # Puma has no singletons, so we need to grab a reference to its CLI object.
+      # The CLI will not exist if puma was invoked through rackup.
+      #
+      # Since the master forks its workers, the CLI object also exists in workers.
+      # By the time this code runs, we'll always be in a worker.
       cli = nil
-      ObjectSpace.each_object(::Puma::CLI) { |o| cli = o }
+      ObjectSpace.each_object(::Puma::CLI) { |o| cli = o } if defined?(::Puma::CLI)
 
-      raise RuntimeError, "Puma does not appear to be active; no instances of Puma::CLI reside in memory" unless cli
-
-      if cli.clustered? && cli.options[:workers] > 0
-        # We're clustered, so workers can SIGTERM themselves to cause a restart
+      if cli && cli.clustered? && cli.options[:workers] > 0
+        # Multiprocess model: send ourselves SIGTERM
         super(app, :TERM, :QUIT, min, max, cycle, verbose)
       else
-        # We're running in single mode; our one-and-only process must USR2 to restart
-        # itself
+        # Single-process model ("rackup," or "puma -w 0")
         super(app, :USR2, :USR1, min, max, cycle, verbose)
       end
     end
